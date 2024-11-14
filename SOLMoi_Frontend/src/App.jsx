7e23e58965a381e0e35ed022d6de3ed03107f182
J2Jayy <betatest0710@gmail.com>
2024-11-14 10:17:22 +0900

    [INIT] Set React

---
+++ SOLMoi_Frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { mainRoutes } from "./routers/main-router";
+
+function renderRoutes(routesObj) {
+  return routesObj.map((route) => {
+    if (route.children) {
+      return (
+        <Route
+          key={route.path}
+          path={route.path}
+          index={route.index}
+          element={route.element}
+        >
+          {route.children ? renderRoutes(route.children) : null}
+        </Route>
+      );
+    }
+    return (
+      <Route
+        key={route.path}
+        path={route.path}
+        index={route.index}
+        element={route.element}
+      />
+    );
+  });
+}
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>{renderRoutes(mainRoutes)}</Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;

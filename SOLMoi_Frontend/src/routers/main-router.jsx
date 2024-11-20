a49be7fe5ab391467fa1913898d172dbb9545236
J2Jayy <betatest0710@gmail.com>
2024-11-20 11:38:05 +0900

    [FEAT]mainpage_mockup_done

--- SOLMoi_Frontend/src/routers/main-router.jsx
+++ SOLMoi_Frontend/src/routers/main-router.jsx
@@ -1,11 +1,17 @@
 // Router: 각 URL에 따른 page 컴포넌트 연결
 import { createBrowserRouter } from "react-router-dom";
 import InvestMainBoard from "../routes/investMainBoard";
+import UpdownGameBoard from "../routes/UpdownGameBoard";
 
 export const mainRoutes = [
   {
-    path: "/",
+    path: "/stockmain",
     element: <InvestMainBoard></InvestMainBoard>,
+    index: true,
+  },
+  {
+    path: "/updowngame",
+    element: <UpdownGameBoard></UpdownGameBoard>,
     index: true,
   },
 ];

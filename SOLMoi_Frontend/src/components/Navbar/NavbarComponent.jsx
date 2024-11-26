c5a420acd88fd3b0cef6c216d7ddab08d9aacacb
MinYeong-mkk <kmy26377538@gmail.com>
2024-11-27 08:49:18 +0900

    attendanceComponent.jsx Style 수정

--- SOLMoi_Frontend/src/components/Navbar/NavbarComponent.jsx
+++ SOLMoi_Frontend/src/components/Navbar/NavbarComponent.jsx
@@ -20,7 +20,7 @@ export default function NavbarComponent() {
     }, [setEmail, setOnLogin]);
 
     return (
-        <Navbar fixed="top" expand={expand} className="bg-body-tertiary mb-3" style={{ maxWidth: "400px" }}>
+        <Navbar expand={expand} className="bg-body-tertiary" style={{  borderBottom: "1px solid #e9e9e9",  }}>
             <Container fluid className="custom-navbar">
                 <Navbar.Brand href="/">쏠모이</Navbar.Brand>
                 <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />

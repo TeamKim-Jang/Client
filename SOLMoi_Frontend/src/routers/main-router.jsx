8fc641af81c5fd4327127461d6f9108273ede062
ehdwoKIM <kinbell19@gmail.com>
2024-12-02 00:09:08 +0900

    feat: 학교 내 랭킹 페이지 구현 & 컴포넌트별 로직 분리

--- SOLMoi_Frontend/src/routers/main-router.jsx
+++ SOLMoi_Frontend/src/routers/main-router.jsx
@@ -5,33 +5,37 @@ import RegisterBoard from "../routes/registerBoard";
 import AttendanceCheckBoard from "../routes/AttendanceCheckBoard";
 import InvestMainBoard from "../routes/InvestMainBoard";
 import OverallRanking from '../components/ranking/OverallRanking';
-
-
+import SchoolRanking from '../components/ranking/SchoolRanking';
 
 export const mainRoutes = [
   {
     path: "/",
-    element: <InvestMainBoard></InvestMainBoard>,
+    element: <InvestMainBoard />,
     index: true,
   },
   {
     path: "/auth/login",
-    element: <LoginBoard></LoginBoard>,
+    element: <LoginBoard />,
     index: true,
   },
   {
     path: "/auth/register",
-    element: <RegisterBoard></RegisterBoard>,
+    element: <RegisterBoard />,
     index: true,
   },
   {
-    path:"/attendance",
-    element: <AttendanceCheckBoard></AttendanceCheckBoard>,
+    path: "/attendance",
+    element: <AttendanceCheckBoard />,
     index: true,
   },
   {
-    path:"/ranking/overall",
-    element: <OverallRanking></OverallRanking>,
+    path: "/ranking/overall",
+    element: <OverallRanking />,
+    index: true,
+  },
+  {
+    path: "/ranking/school/:schoolId",
+    element: <SchoolRanking />,
     index: true,
   },
 ];

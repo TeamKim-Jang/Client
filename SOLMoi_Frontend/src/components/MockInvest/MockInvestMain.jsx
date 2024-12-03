ebab9e2362ce68df0384960a0c66585fe7fd3c09
MinYeong-mkk <kmy26377538@gmail.com>
2024-12-03 14:37:52 +0900

    fix: user 인증 방식 수정

--- SOLMoi_Frontend/src/components/MockInvest/MockInvestMain.jsx
+++ SOLMoi_Frontend/src/components/MockInvest/MockInvestMain.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 const formatNumber = (num) => {
   return new Intl.NumberFormat("ko-KR").format(num);
